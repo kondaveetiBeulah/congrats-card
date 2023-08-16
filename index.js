beecccbb135a87e85c7a10fe99e85d44d247b4d3
Your Name <you@example.com>
2023-08-16 08:18:45 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const element = (
+  <div className="bg-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="personCard">
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png" />
+      <h1>Kiran</h1>
+      <p>
+        Vishnu institute of computer science Education and Technology,
+        Bhimavaram
+      </p>
+      <img src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png" />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
